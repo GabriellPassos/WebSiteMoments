3a608e30c526a7eb679d3b1a95e9bbbfc9cf142d
Gabriel Passos <gabrielsilvapassos42@gmail.com>
2023-04-29 20:44:07 -0300

    Adição de formulário para envio de momentos

--- src/app/components/moment-form/moment-form.component.ts
+++ src/app/components/moment-form/moment-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-moment-form',
   templateUrl: './moment-form.component.html',
@@ -7,4 +7,25 @@ import { Component, Input } from '@angular/core';
 })
 export class MomentFormComponent {
   @Input() btnText!: string
+  ngOnInit():void{
+    this.momentForm = new FormGroup({
+      id: new FormControl(''),
+      title: new FormControl('', [Validators.required]),
+      description: new FormControl('',[Validators.required]),
+      image: new FormControl(''),
+    });
+  }
+  get title(){
+    return this.momentForm.get('title')!;
+  }
+  get description(){
+    return this.momentForm.get('description')!;
+  }
+  momentForm!:FormGroup
+  submit(){
+    if(this.momentForm.invalid){
+      return;
+    }
+    console.log("enviou formulario")
+  }
 }
