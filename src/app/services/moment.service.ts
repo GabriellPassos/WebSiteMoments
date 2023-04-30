e0e3ee2481b5593e5dfe1109170cf6aed8c9bbd2
Gabriel Passos <gabrielsilvapassos42@gmail.com>
2023-04-30 12:35:34 -0300

    preenchimento com momentos da pagina home

--- src/app/services/moment.service.ts
+++ src/app/services/moment.service.ts
@@ -3,6 +3,9 @@ import { HttpClient } from '@angular/common/http';
 import {Observable} from 'rxjs';
 import { Moment } from '../Moment';
 import { environment } from 'src/environments/environment';
+import { Response } from '../Response';
+
+
 @Injectable({
   providedIn: 'root'
 })
@@ -10,6 +13,9 @@ export class MomentService {
   private baseApiUrl = environment.baseApiUrl;
   private apiUrl = `${this.baseApiUrl}api/moments`;
   constructor(private http:HttpClient) {}
+  getMoments():Observable<Response<Moment[]>>{
+    return this.http.get<Response<Moment[]>>(this.apiUrl);
+  }
   createMoment(formData:FormData):Observable<FormData>{
     return this.http.post<FormData>(this.apiUrl, formData);
   }
